0e7bbc40badfdd1aeb67d9f9e8815c0358eac74a
Ryudai Iwakami <103726264+RyudaiIwakami@users.noreply.github.com>
2024-04-15 18:21:18 +0900

    add chmod command

--- test_all.sh
+++ test_all.sh
@@ -2,38 +2,47 @@
 
 # ex01_1
 echo "Start testing ex01_1.c"
+chmod +x locked/ex01_1.sh
 ./locked/ex01_1.sh
 
 # ex01_2
 echo "Start testing ex01_2.c"
+chmod +x locked/ex01_2.sh
 ./locked/ex01_2.sh
 
 # ex01_3
 echo "Start testing ex01_3.c"
+chmod +x locked/ex01_3.sh
 ./locked/ex01_3.sh
 
 # ex01_4
 echo "Start testing ex01_4.c"
+chmod +x locked/ex01_4.sh
 ./locked/ex01_4.sh
 
 # ex01_5
 echo "Start testing ex01_5.c"
+chmod +x locked/ex01_5.sh
 ./locked/ex01_5.sh
 
 # ex01_6
-echo "Start testing ex01_6.c"
+echo "Start testing ex01_6.c"4
+chmod +x locked/ex01_6.sh
 ./locked/ex01_6.sh
 
 # ex01_7
 echo "Start testing ex01_7.c"
+chmod +x locked/ex01_7.sh
 ./locked/ex01_7.sh
 
 # ex01_8
 echo "Start testing ex01_8.c"
+chmod +x locked/ex01_8.sh
 ./locked/ex01_8.sh
 
 # ex01_9
 echo "Start testing ex01_9.c"
+chmod +x locked/ex01_9.sh
 ./locked/ex01_9.sh
 
 # 終了
